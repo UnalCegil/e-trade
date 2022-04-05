a46450f00852c3ec474d9c4bc901bb74ed469657
UnalCegil <72045966+UnalCegil@users.noreply.github.com>
2022-04-05 11:17:26 +0300

    first commit

---
+++ src/component/About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <div className="container py-5 my-5">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="text-primary fw-bold mb-4">About Us</h1>
+            <p className="lead mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              molestiae earum rem doloremque, nihil delectus ullam error
+              consectetur? 
+            </p>
+            <NavLink to="/contact" className="btn btn-outline-primary px-3">
+              Contact Us
+            </NavLink>
+          </div>
+       
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
